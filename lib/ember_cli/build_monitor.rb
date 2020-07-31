551b69eed6dab53d7dcdaa96bf5f8b84050de13b
Carmen Wick <carmen@incloudcounsel.com>
2020-07-31 15:15:51 -0700

    Exclude status messages from build errors

--- lib/ember_cli/build_monitor.rb
+++ lib/ember_cli/build_monitor.rb
@@ -42,7 +42,8 @@ module EmberCli
     def build_errors
       error_lines.
         reject { |line| is_blank_or_backtrace?(line) }.
-        reject { |line| is_deprecation_warning?(line) }
+        reject { |line| is_deprecation_warning?(line) }.
+        reject { |line| is_status_message?(line) }
     end
 
     def has_build_errors?
@@ -55,6 +56,10 @@ module EmberCli
 
     def is_deprecation_warning?(line)
       line =~ /^(\e[^\s]+)?DEPRECATION:/
+    end
+
+    def is_status_message?(line)
+      line =~ /^-.+Building.+|^- cleaning up$/i
     end
 
     def error_lines
